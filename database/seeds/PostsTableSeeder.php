b3d6b694054efce84e957cacd4334a7e601c8725
Sabrina <sabrinacunsolo85@gmail.com>
2021-01-25 17:27:09 +0100

    risolto problema

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -16,12 +16,12 @@ class PostsTableSeeder extends Seeder
 
         for ($i=0; $i <49 ; $i++) {
             $new_post = new Post();
-            $new_post->title = $faker->word();
+            $new_post->title    = $faker->word();
             $new_post->subtitle = $faker->sentence();
-            $new_post->text = $faker->text();
-            $new_post->author = $faker->name();
-            $new_post->date = $faker->date();
-            $new_post ->save();
+            $new_post->text     = $faker->text();
+            $new_post->author   = $faker->name();
+            $new_post->date     = $faker->date();
+            $new_post->save();
         }
 
     }
